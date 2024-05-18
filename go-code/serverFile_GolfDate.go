882a838a77e11862bd2e85630708b7d7d5a3b9e6
kyle2501@gmail.com <kyle2501@gmail.com>
2024-05-17 18:32:04 -0700

    update

---
+++ go-code/serverFile_GolfDate.go
@@ -0,0 +1,28 @@
+# GolfDate-app ~
+
+package main
+
+
+
+import (
+    
+    
+)
+
+
+func app_welcome_center_page() {
+    
+    
+}
+
+func app_infornation_booth_page() {
+    
+    
+}
+
+func app_login_center_page() {
+    
+    
+}
+
+
